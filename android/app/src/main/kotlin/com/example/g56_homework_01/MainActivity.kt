a297a82298877a5688e36f1fa2016b7850a02b41
Hugo Angeles <hugoangeles0810@gmail.com>
2021-10-14 10:17:48 -0500

    Project created

---
+++ android/app/src/main/kotlin/com/example/g56_homework_01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.g56_homework_01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'package:g56_homework_01/src/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
